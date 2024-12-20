884cf9124a0237b5f8ffd70567684d1b28b68aa0
Joya <jorgejoya.100@gmail.com>
2024-12-20 10:05:06 -0600

    pagina de administrador

--- src/components/CountChart.tsx
+++ src/components/CountChart.tsx
@@ -9,14 +9,19 @@ import {
 
 const data = [
   {
+    name: "Total",
+    count: 106,
+    fill: "white",
+  },
+  {
     name: "Girls",
     count: 53,
-    fill: "#8884d8",
+    fill: "#FAE27C",
   },
   {
     name: "Boys",
     count: 53,
-    fill: "#83a6ed",
+    fill: "#C3EBFA",
   },
 ];
 // const style = {
@@ -35,28 +40,24 @@ const CountChart = () => {
         <Image src="/moreDark.png" alt='' width={20} height={20}/>
       </div>
       {/* CHART */}
-      <div className="w-full h-[75% ]">
+      <div className="relative w-full h-[75%]">
         <ResponsiveContainer>
           <RadialBarChart
             cx="50%"
             cy="50%"
-            innerRadius="10%"
-            outerRadius="80%"
-            barSize={10}
+            innerRadius="40%"
+            outerRadius="100%"
+            barSize={32}
             data={data}
           >
             <RadialBar
-              label={{ position: "insideStart", fill: "#fff" }}
               background
-              dataKey="uv"
-            />
-            <Legend
-              iconSize={10}
-              layout="vertical"
-              verticalAlign="middle"
+              dataKey="count"
             />
           </RadialBarChart>
         </ResponsiveContainer>
+        <Image src="/maleFemale.png" alt='' width={50} height={50} className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'/>
+       
       </div>
       {/* BOTTOM */}
       <div className="flex justify-center gap-16">
@@ -76,3 +77,6 @@ const CountChart = () => {
 };
 
 export default CountChart;
+
+
+
